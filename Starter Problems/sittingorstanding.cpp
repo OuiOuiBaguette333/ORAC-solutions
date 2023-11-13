5c3fce324262f707f66860878055cce1a7656cdc
OuiOuiBaguette333 <148035758+OuiOuiBaguette333@users.noreply.github.com>
2023-11-13 16:11:24 +1100

    Create sittingorstanding.cpp

---
+++ Starter Problems/sittingorstanding.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+#define speed() \
+ios::sync_with_stdio(false); \
+cin.tie(0)
+#ifndef DEBUG
+#define open(name) \
+speed(); \
+freopen(#name "in.txt", "r", stdin); \
+freopen(#name "out.txt", "w", stdout)
+#else
+#define open(name) speed()
+#endif
+#define all(x) (x).begin(), (x).end()
+#define _forI(var, start, end) for (int var = start; var < end; var++)
+ 
+int main() {
+        open(sit);
+ 
+        int r, s, t;
+ 
+        cin >> r >> s >> t;
+ 
+        int ans = min(t, r * s);
+ 
+        cout << ans << ' ' << t - ans;
+        
+        return 0;
+}
